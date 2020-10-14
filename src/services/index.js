9fb6c5578aee568e3712a0c223ced6547957e63d
aaronllcam <aaronllcam@gmail.com>
2020-10-14 17:32:10 +0200

    ERRors resolve

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,8 @@ module.exports = {
     HomeService: require('./home.service'),
     UserService: require('./user.service'),
     IdeaService: require('./idea.service'),
-    CommentService: require('./comment.service')
+    CommentService: require('./comment.service'),
+    AuthService: require('./auth.service')
 
 
 }
--- src/startup/container.js
+++ src/startup/container.js
@@ -6,13 +6,13 @@ const { createContainer, asClass, asValue, asFunction } = require('awilix');
 const config = require("../config")
 const app = require(".")
 //services
-const { HomeService, CommentService, IdeaService, UserService } = require('../services')
+const { HomeService, CommentService, IdeaService, UserService, AuthService } = require('../services')
 
 //controllers
-const { HomeController, CommentController, IdeaController, UserController } = require('../controllers')
+const { HomeController, CommentController, IdeaController, UserController, AuthController } = require('../controllers')
 
 //routes
-const { HomeRoutes, CommentRoutes, IdeaRoutes, UserRoutes } = require('../routes/index.routes')
+const { HomeRoutes, CommentRoutes, IdeaRoutes, UserRoutes, AuthRoutes } = require('../routes/index.routes')
 const Routes = require('../routes')
 
 //repositories
@@ -33,19 +33,22 @@ container
     HomeService: asClass(HomeService).singleton(),
     UserService: asClass(UserService).singleton(),
     IdeaService: asClass(IdeaService).singleton(),
-    CommentService: asClass(CommentService).singleton()
+    CommentService: asClass(CommentService).singleton(),
+    AuthService: asClass(AuthService).singleton()
 
 
 }).register({
     HomeController: asClass(HomeController.bind()).singleton(),
     UserController: asClass(UserController.bind()).singleton(),
     IdeaController: asClass(IdeaController.bind()).singleton(), 
-    CommentController: asClass(CommentController.bind()).singleton()
+    CommentController: asClass(CommentController.bind()).singleton(),
+    AuthController: asClass(AuthController.bind()).singleton(),
 }).register({
     HomeRoutes: asFunction(HomeRoutes).singleton(),
     UserRoutes: asFunction(UserRoutes).singleton(),
     IdeaRoutes: asFunction(IdeaRoutes).singleton(),
-    CommentRoutes: asFunction(CommentRoutes).singleton()
+    CommentRoutes: asFunction(CommentRoutes).singleton(),
+    AuthRoutes: asFunction(AuthRoutes).singleton()
 }).register({
     User: asValue(User),
     Idea: asValue(Idea),
